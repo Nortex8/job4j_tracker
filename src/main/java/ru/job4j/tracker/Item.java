3a7455de06e4a70b65e9806e0dddf4c09425a442
Nortex8 <ivanov9y@gmail.com>
2022-06-17 14:44:41 +0300

    2. Перегрузка конструктора [#173890 #313225]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -21,14 +21,14 @@ public class Item {
         this.name = name;
     }
 
-    public Item() {
-    }
-
     public int getId() {
         return id;
     }
 
     public String getName() {
         return name;
+    }
+
+    public Item() {
     }
 }
