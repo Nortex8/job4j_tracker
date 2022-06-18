e486c26ac13886344dcc1fb0a5af0e8a93a86ba4
Nortex8 <ivanov9y@gmail.com>
2022-06-18 20:05:24 +0300

    7. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,11 +3,10 @@ package ru.job4j.tracker;
 import java.time.format.DateTimeFormatter;
 
 public class StartUI {
+
     public static void main(String[] args) {
         Item item = new Item();
-        System.out.println(item.getCreated());
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(item.getCreated().format(formatter));
         String currentDateTimeFormat = item.getCreated().format(formatter);
         System.out.println(currentDateTimeFormat);
     }
