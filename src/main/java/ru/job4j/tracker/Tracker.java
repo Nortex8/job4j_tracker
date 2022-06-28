b6678a7716208120f61dab2d3e74ea70836323bd
Nortex8 <ivanov9y@gmail.com>
2022-06-28 12:19:20 +0300

    5. Tracker - хранилище [#396 #316201]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -22,7 +22,7 @@ public class Tracker {
         int count = 0;
         for (int i = 0; i < size; i++) {
             Item item = items[i];
-            if (item != null && item.getName().equals(key)) {
+            if (item.getName().equals(key)) {
                 rsl[i] = item;
                 count++;
             }
