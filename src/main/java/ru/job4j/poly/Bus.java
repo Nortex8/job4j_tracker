57c4646418a492e6849ed9711b8626b2b5583455
Nortex8 <ivanov9y@gmail.com>
2022-07-09 14:40:21 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,20 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passenger(int number) {
+
+    }
+
+    @Override
+    public int fuel(int amountFuel) {
+        int pricePerLiter = 60;
+        return amountFuel * pricePerLiter;
+    }
+}
