0039508cb5d1946197a8b36406cc8a67f59f3c34
Nortex8 <ivanov9y@gmail.com>
2022-07-11 18:43:02 +0300

    /

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -29,17 +29,4 @@ public class StartUITest {
         Item edited = tracker.findById(item.getId());
         assertThat(edited.getName(), is("edited item"));
     }
-
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId())
-        };
-        StartUI.deleteItem(new StubInput(answers), tracker);
-        Item deleted = tracker.findById(item.getId());
-        assertThat(deleted.getName(), is("null"));
-    }
 }
