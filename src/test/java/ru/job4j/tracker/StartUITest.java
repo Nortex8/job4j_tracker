e7b69f4ad190da4cabe97db2212b03d981cf9c1b
Nortex8 <ivanov9y@gmail.com>
2022-07-11 15:35:32 +0300

    5.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -17,13 +17,29 @@ public class StartUITest {
     }
 
     @Test
-    public void whenCreateItem() {
-        String[] answers = {"Test for what"};
-        Input input = new StubInput(answers);
+    public void whenEditItem() {
         Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Test for what");
-        assertThat(created.getName(), is(expected.getName()));
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "edited item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName(), is("edited item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId())
+        };
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        assertThat(deleted.getName(), is("null"));
     }
 }
