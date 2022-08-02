bde71e1961afa52bd7a22d1961537d2ce75d3bfd
Nortex8 <ivanov9y@gmail.com>
2022-08-02 14:52:12 +0300

    2. Шаблон фабричный метод [#95067 #327003] lection

---
+++ src/main/java/ru/job4j/io/Canvas.java
@@ -0,0 +1,17 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+
+public class Canvas {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите ширину прямоугольника: ");
+        int a = Integer.parseInt(input.nextLine());
+        System.out.print("Введите длину прямоугольника: ");
+        int b = Integer.parseInt(input.nextLine());
+        Rectangle rectangle = new Rectangle(a, b);
+        System.out.println(rectangle.draw());
+        System.out.println("Площадь прямоугольника равна: " + rectangle.square());
+    }
+}
